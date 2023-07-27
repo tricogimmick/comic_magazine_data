5a7108eae4a94970b5c1602fc02cd4a07a51d65c
Tricogimmick <nomura@tricogimmick.com>
2023-07-27 15:53:31 +0900

    Remove Astro Projects

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-    site: 'https://tricogimmick.github.io',
-    base: '/comic_magazine_data'
-});
