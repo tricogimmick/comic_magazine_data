fd85b3fe27046317438cd3326cdff6898af8ee9e
Tricogimmick <nomura@tricogimmick.com>
2023-09-20 10:32:15 +0900

    2023-09-20 Database 更新

--- database/issues.sql
+++ database/issues.sql
@@ -461,4 +461,6 @@ INSERT INTO issues VALUES('WSSD202342','WSSD','2023年 No.42','https://i.gyazo.c
 INSERT INTO issues VALUES('WMRG202342','WMRG','2023年 No.42','https://i.gyazo.com/0cffeca015252e024ce6e00cc3cd08d2.png','https://i.gyazo.com/c0f9ac3b75b24a874298e3ad09542b5d.png','');
 INSERT INTO issues VALUES('WSJP202342','WSJP','2023年 No.42','https://i.gyazo.com/22a69509d612f1b972daee5ba7980b86.png','https://i.gyazo.com/ab335b4a503ada6d28b9d848c234a81e.png','');
 INSERT INTO issues VALUES('WYMG202342','WYMG','2023年 No.42','https://i.gyazo.com/f04b8e51c6e6d54fcd376755f6f13197.png','https://i.gyazo.com/469cf34ae7c3fa7352709b0536fd5240.png','');
+INSERT INTO issues VALUES('WSMG202342','WSMG','2023年 No.42','https://i.gyazo.com/4bdd7ba4a456a4b99f9f02735d4c9ab7.png','https://i.gyazo.com/fe03d8ac074ed4b3a5b6de7e2590e42b.png','');
+INSERT INTO issues VALUES('WSSD202343','WSSD','2023年 No.43','https://i.gyazo.com/33242972910133b887b24fd8aab6c587.png','https://i.gyazo.com/491da1359cb266bc1407ed49dbbab2f2.png','');
 COMMIT;
--- database/titles.sql
+++ database/titles.sql
@@ -703,4 +703,7 @@ INSERT INTO titles VALUES(706,'委員長とつむじ風',0,'WSSD',0,'週刊少�
 INSERT INTO titles VALUES(707,'クサリスカル',0,'WSMG',0,'週刊少年マガジン　2023年 No.41に掲載');
 INSERT INTO titles VALUES(708,'彼氏時々彼女',1,'BSMG',0,'週刊少年マガジン 2023年 No.41に出張掲載');
 INSERT INTO titles VALUES(709,'カグラバチ',1,'WSJP',0,'週刊少年ジャンプ 2023年 No.42より連載開始');
+INSERT INTO titles VALUES(710,'亡者絶対殺す',0,'WSSD',0,'週刊少年サンデー 2023年 No.43に掲載');
+INSERT INTO titles VALUES(711,'ミモザイズム',1,'ISDW',0,'週刊少年サンデー 2023年 No.43 に出張掲載');
+INSERT INTO titles VALUES(712,'真夜中ハートチューン',1,'WSMG',0,'週刊少年マガジン 2023年 No.42より連載開始');
 COMMIT;
