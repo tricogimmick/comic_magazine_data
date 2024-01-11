2454087a648ef363cdc5f83d0600fbc3f4ca2765
Tricogimmick <nomura@tricogimmick.com>
2024-01-11 10:00:13 +0900

    2024-01-11 Database 更新

--- database/issues.sql
+++ database/issues.sql
@@ -538,4 +538,5 @@ INSERT INTO issues VALUES('WYMG202406','WYMG','2024年 No.06','https://i.gyazo.c
 INSERT INTO issues VALUES('WSJP202406-07','WSJP','2024年 No.06・07合併号','https://i.gyazo.com/c01cc2fe20921130bdf5c96a9d4c8385.png','https://i.gyazo.com/9fdfb4b19248f40e283197884085e5c0.png','');
 INSERT INTO issues VALUES('WSSD202407','WSSD','2024年 No.07','https://i.gyazo.com/57dad2fb72d262c0acbb84581d191d56.png','https://i.gyazo.com/9b4d4b19aa82245a4bc8d4379fa781dc.png','');
 INSERT INTO issues VALUES('WSMG202406','WSMG','2024年 No.06','https://i.gyazo.com/2d0b337c8cc4153306212f2c5d681314.png','https://i.gyazo.com/c120f4e5079d869413dc3716b822a493.png','');
+INSERT INTO issues VALUES('WMRG202406','WMRG','2024年 No.06','https://i.gyazo.com/76287b9bef2a0c79ec7a90293d339fa4.png','https://i.gyazo.com/e509b0bc93503f64f8554f9b6e9377e7.png','');
 COMMIT;
