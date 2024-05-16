b3f9c3711593dc6e4edb51921cc17fd0bf4055a1
Tricogimmick <nomura@tricogimmick.com>
2024-05-16 11:31:05 +0900

    2024-05-16 Database Update

--- database/issues.sql
+++ database/issues.sql
@@ -626,4 +626,5 @@ INSERT INTO issues VALUES('WYMG202424','WYMG','2024年 No.24','https://i.gyazo.c
 INSERT INTO issues VALUES('WSJP202424','WSJP','2024年 No.24','https://i.gyazo.com/c92e0e1407d75ea3dcde78186fe1614c.png','https://i.gyazo.com/c8cf74aefb7b4ff0569ca1224b034b67.png','');
 INSERT INTO issues VALUES('WSMG202424','WSMG','2024年 No.24','https://i.gyazo.com/c740482b722e5819b23cbb9a50e86b8e.png','https://i.gyazo.com/252b95603f3d9850dc8f6c7f41a2d366.png','');
 INSERT INTO issues VALUES('WSSD202425','WSSD','2024年 No.25','https://i.gyazo.com/9f5775f36df47156f8f7a57f852a6857.png','https://i.gyazo.com/4d7718d3c2c279be8157e28f4c45378a.png','');
+INSERT INTO issues VALUES('WMRG202424','WMRG','2024年 No.24','https://i.gyazo.com/324e7c340c647837affc9a56cc65af4b.png','https://i.gyazo.com/22e41f3da179e802ef8ea38fe07b024c.png','');
 COMMIT;
